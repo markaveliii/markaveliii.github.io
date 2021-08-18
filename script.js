8c70f288a26109df96c0fceac536b7f2693ef494
Mark Montes <mark.montes44@gmail.com>
2021-08-17 21:55:01 -0700

    added to layout, created script, implemented styles

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('scroll', () => {
+	let content = document.querySelector('.body1');
+	let contentPosition = content.getBoundingClientRect().top;
+	let contentPositionBot = content.getBoundingClientRect().bottom/2;
+	let screenPosition = window.innerHeight;
+
+	if(contentPosition < screenPosition){
+		content.classList.add('body-animation');
+		if(contentPositionBot < screenPosition){
+			$('.body1-content').show();
+		}
+	}
+	else{
+		content.classList.remove('body-animation');
+		$('.body1-content').hide();
+	}
+})
+
+window.addEventListener('scroll', () => {
+	let content = document.querySelector('.body2');
+	let contentPosition = content.getBoundingClientRect().top;
+	let screenPosition = window.innerHeight;
+
+	if(contentPosition < screenPosition){
+		content.classList.add('body-animation');
+		$('.body2-content').show();
+	}
+	else{
+		content.classList.remove('body-animation');
+		$('.body2-content').hide();
+	}
+})
+
+window.addEventListener('scroll', () => {
+	let content = document.querySelector('.body3');
+	let contentPosition = content.getBoundingClientRect().top;
+	let screenPosition = window.innerHeight;
+
+	if(contentPosition < screenPosition){
+		content.classList.add('body-animation');
+		$('.body3-content').show();
+	}
+	else{
+		content.classList.remove('body-animation');
+		$('.body-content').hide();
+	}
+})
+
